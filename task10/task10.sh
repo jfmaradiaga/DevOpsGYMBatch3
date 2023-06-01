4b1e565564642d64adf7ea3fe84d086f74ee2d3f
Julio Maradiaga <juliomaradiagaa@gmail.com>
2023-05-31 19:59:09 -0600

    Update Docker image

--- task10/task10.sh
+++ task10/task10.sh
@@ -1,5 +1,12 @@
-
 #!/bin/bash
+#set -e
+
+# go to the git repo path
+cd /home/jmaradiaga/DevOps/DevOpsGYMBatch3/
+
+# Pull the lastest changes
+echo "Pulling the latest changes from the remote repository..."
+ssh-agent bash -c 'ssh-add /home/jmaradiaga/.ssh/id_rsa; git pull'
 
 # Install Docker
 sudo apt-get update
@@ -15,8 +22,26 @@ sudo apt-get install -y golang
 cd /home/jmaradiaga/DevOps/DevOpsGYMBatch3/
 go build -o /home/jmaradiaga/DevOps/DevOpsGYMBatch3/hello /home/jmaradiaga/DevOps/DevOpsGYMBatch3/hello.go
 
-# Create the Docker image
+# Create/build the Docker image
 sudo docker build -t jfmaradiaga/helloworld:latest /home/jmaradiaga/DevOps/DevOpsGYMBatch3/
+
+# Log in to Docker Hub or your desired Docker registry
+echo "Logging in to the Docker registry..."
+docker login -u jfmaradiaga -p dckr_pat_dp-A2vXuxznjjdyUWqiBnLlV2yU
 
 # Push the Docker image to the Docker repository
 sudo docker push jfmaradiaga/helloworld:latest
+
+# Add changes to the local repository
+echo "Adding changes to the local repository..."
+git add .
+
+# Commit the changes
+echo "Committing changes..."
+git commit -m "Update Docker image"
+
+# Push the changes to the remote repository
+echo "Pushing changes to the remote repository..."
+ssh-agent bash -c 'ssh-add /home/jmaradiaga/.ssh/id_rsa; git push'
+
+echo "Script execution completed."
