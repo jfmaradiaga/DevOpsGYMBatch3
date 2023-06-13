ea5157c0117c420239f87ccea1f6a97b0a90748f
Julio Maradiaga <juliomaradiagaa@gmail.com>
2023-06-12 22:42:40 -0600

    Update Docker image

--- hello.go
+++ hello.go
@@ -6,7 +6,7 @@ import (
 )
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello, AWS EC2 World!")
+	fmt.Fprintln(w, "Hello, neat AWS EC2 World!")
 }
 
 func main() {
