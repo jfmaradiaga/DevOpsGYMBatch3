343833d2f23ba3edb21a72b1d4393177a83b6b9e
Julio Maradiaga <juliomaradiagaa@gmail.com>
2023-06-12 23:02:44 -0600

    Update Docker image

--- hello.go
+++ hello.go
@@ -1,19 +1,15 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
+        "fmt"
+        "net/http"
 )
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
-<<<<<<< HEAD
-	fmt.Fprintln(w, "Hello, neat AWS EC2 World!")
-=======
-	fmt.Fprintln(w, "Hello, Neat AWS EC2 World!")
->>>>>>> 54697c47ae3a0e7b57b098426d98f7359184159b
+        fmt.Fprintln(w, "Hello, Neat AWS EC2 World!")
 }
 
 func main() {
-	http.HandleFunc("/", helloHandler)
-	http.ListenAndServe(":8080", nil)
+        http.HandleFunc("/", helloHandler)
+        http.ListenAndServe(":8080", nil)
 }
