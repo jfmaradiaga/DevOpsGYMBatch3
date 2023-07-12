4137ff5149626a06b90ac5359c38de0fc4d784c0
Julio Maradiaga <juliomaradiagaa@gmail.com>
2023-07-11 20:32:39 -0600

    task15Completed

--- task15/task15/lib/main.dart
+++ task15/task15/lib/main.dart
@@ -1,6 +1,23 @@
-import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Hello World',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HelloWorldScreen(),
+    );
+  }
+}
 
 class HelloWorldScreen extends StatefulWidget {
   @override
