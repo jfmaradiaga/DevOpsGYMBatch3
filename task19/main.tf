aa19f23148a4f9b0cf716cc2551d0376c94914e7
Julio Maradiaga <juliomaradiagaa@gmail.com>
2023-10-05 20:41:16 -0600

    tf files formated

--- task19/main.tf
+++ task19/main.tf
@@ -5,6 +5,7 @@ terraform {
       version = "~> 5.0"
     }
   }
+
   backend "s3" {
     key = "aws/ec2-deploy/terraform.tfstate"
   }
@@ -13,18 +14,20 @@ terraform {
 provider "aws" {
   region = var.region
 }
+
 resource "aws_instance" "public_instance" {
   ami                    = "ami-053b0d53c279acc90"
   instance_type          = "t2.micro"
   key_name               = aws_key_pair.key_pair.key_name
-  vpc_security_group_ids = [ aws_security_group.sg_ec2 ]
-  iam_instance_profile = aws_iam_instance_profile.ec2-profile.name
+  vpc_security_group_ids = [aws_security_group.sg_ec2]
+  iam_instance_profile   = aws_iam_instance_profile.ec2-profile.name
+
   connection {
-    type = "ssh"
-    host = self.public_ip
-    user = "ubuntu"
+    type        = "ssh"
+    host        = self.public_ip
+    user        = "ubuntu"
     private_key = var.private_key
-    timeout = "4m"
+    timeout     = "4m"
   }
 
   tags = {
@@ -36,12 +39,11 @@ resource "aws_iam_instance_profile" "ec2-profile" {
   name = "ec2-profile"
   role = "EC2-ECR-AUTH"
 }
-# Create a security group
+
 resource "aws_security_group" "sg_ec2" {
   name        = "sg_ec2"
   description = "Security group for EC2"
 
-  # SSH rule
   ingress {
     from_port   = 22
     to_port     = 22
@@ -49,7 +51,6 @@ resource "aws_security_group" "sg_ec2" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  # HTTP (Port 80) rule
   ingress {
     from_port   = 80
     to_port     = 80
@@ -65,13 +66,12 @@ resource "aws_security_group" "sg_ec2" {
   }
 }
 
-// Create Key Pair for Connecting EC2 via SSH
 resource "aws_key_pair" "key_pair" {
   key_name   = var.key_name
   public_key = var.public_key
 }
 
 output "instance_public_ip" {
-  value = aws_instance.public_instance.public_ip
+  value     = aws_instance.public_instance.public_ip
   sensitive = true
 }
--- task19/variables.tf
+++ task19/variables.tf
@@ -1,12 +1,16 @@
 variable "region" {
-  default = "us-east-1"
+  description = "The AWS region where resources will be created."
+  default     = "us-east-1"
 }
+
 variable "public_key" {
-  
+  description = "The public SSH key to use for EC2 instances."
 }
+
 variable "private_key" {
-  
+  description = "The private SSH key to use for EC2 instances."
 }
+
 variable "key_name" {
-  
+  description = "The name of the SSH key pair for EC2 instances."
 }
