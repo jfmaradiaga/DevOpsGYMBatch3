833a3e266273b3229e6d2325847cfb2fea10322c
Julio Maradiaga <juliomaradiagaa@gmail.com>
2023-10-05 20:55:40 -0600

    key added task19

--- task19/variables.tf
+++ task19/variables.tf
@@ -1,16 +1,12 @@
 variable "region" {
-  description = "The AWS region where resources will be created."
-  default     = "us-east-1"
+  default = "us-east-1"
 }
-
 variable "public_key" {
-  description = "The public SSH key to use for EC2 instances."
+  
 }
-
 variable "private_key" {
-  description = "The private SSH key to use for EC2 instances."
+  
 }
-
 variable "key_name" {
-  description = "The name of the SSH key pair for EC2 instances."
+  
 }
